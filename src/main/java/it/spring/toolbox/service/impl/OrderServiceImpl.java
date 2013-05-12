b20a0dc85ed5cd9f7718830220c53b3e1bdd2e73
adrianaf <adriana.farina23@gmail.com>
2013-05-12 21:48:31 +0200

    Changes to OrderServiceImpl

--- src/main/java/it/spring/toolbox/service/impl/OrderServiceImpl.java
+++ src/main/java/it/spring/toolbox/service/impl/OrderServiceImpl.java
@@ -1,37 +1,38 @@
 package it.spring.toolbox.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import it.spring.toolbox.dao.OrderDAO;
 import it.spring.toolbox.domain.Order;
 import it.spring.toolbox.service.OrderService;
 
-@Service("customerService")
+@Service("orderService")
 @Transactional
 public class OrderServiceImpl implements OrderService {
 
+	@Autowired
+	OrderDAO orderDao;
+	
 	@Override
 	public void save(Order order) {
-		// TODO Auto-generated method stub
-
+		orderDao.save(order);
 	}
 
 	@Override
 	public void update(Order order) {
-		// TODO Auto-generated method stub
-
+		orderDao.update(order);
 	}
 
 	@Override
 	public void delete(Order order) {
-		// TODO Auto-generated method stub
-
+		orderDao.delete(order);
 	}
 
 	@Override
 	public Order findOrderBySeqNo(int sequenceNo) {
-		// TODO Auto-generated method stub
-		return null;
+		return orderDao.findOrderBySeqNo(sequenceNo);
 	}
 
 }
